223c60d316d3b831c864032fd00faa0e3eb96d02
Scotty Wyatt <crashy@Scottys-MacBook-Pro.local>
2021-06-01 15:45:01 -0400

    created like button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import logo from './logo.svg';
+import { Component } from 'react'
 import './App.css';
 
-class Button extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
-    this.state = 
-  }
-  }
-  render() {
-    return (
+    this.state = {
+      count: 0,
+    }
 
-    )
+    this.changeLikes = this.changeLikes.bind(this);
   }
-}
+
+  changeLikes = () => {
+    let newCount = this.state.count + 1
+    this.setState({
+      count: newCount
+    })
+  }
+
+  render() {
+      return (
+
+          <button onClick={this.changeLikes}>{this.state.count} likes</button>
+
+      )
+  }
+  }
+
 
 export default App;
